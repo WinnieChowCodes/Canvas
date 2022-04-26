b90b6070c33e13982035301ee98214b60ae9280e
Winnie Chow <WinnieChow606@outlook.com>
2022-04-26 01:52:03 +0100

    Update gallery.php

--- DigitalCanvas/gallery.php
+++ DigitalCanvas/gallery.php
@@ -9,13 +9,32 @@
 */
 include 'header.php';
 
-echo <<<CREATE
-<html>
-    <head>
-        <title>Gallery</title>
-    </head>
-    <body>
-    </body>
-</html>
-CREATE;
+require_once "functions/connection.php";
+
+//Search query to gather all artwork for the user that is currently logged in  
+$query = "SELECT * FROM `art` LEFT OUTER JOIN users ON art.userID = users.userID";
+$result = mysqli_query($connection,$query);
+$n = mysqli_num_rows($result);
+
+if($n==0){
+    echo "No artworks found!";
+}
+else{
+    //Show all artworks from the database
+    for($i=0;$i<$n;$i++){
+        $row = mysqli_fetch_assoc($result);
+        echo <<<CREATE
+        <div id="artwork" class="card" style="width:20rem;">
+            <img class="card-img-top img-fluid" src="{$row['image']}"/> <br>
+            <div class="card-body">
+                    <h3 class="card-title">{$row['title']}</h3>
+                    <div class="btn-group">
+                        <form method="POST" action="updatePost.php"><button type=submit name="post" value="'{$row['postid']}'" class="btn btn-primary">Update</button></form>
+                        <form method="POST" action="deletePost.php"><button type=submit name="post" value="'{$row['postid']}'" class="btn btn-danger">Delete</button></form>           
+                    </div>
+                </div>
+        </div>
+        CREATE;
+    }
+}
 ?>
